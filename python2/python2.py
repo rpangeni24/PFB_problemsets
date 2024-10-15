15b64cfff799e7526b44d3696b13aa506c471d10
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-15 17:03:00 -0400

    python2.py

--- python2/python2.py
+++ python2/python2.py
@@ -12,10 +12,12 @@ else:
 	print('negative')
 
 number2 = 20
-if number2 > 0:
+if number2 < 0:
 	print('not true')
-elif number > 20:
+elif number2 > 20:
 	print('number is greater than 20')
+elif number2 % 3 == 0:
+	print('number is divisible by 3')
 else:
 	print('number is 20')
 
