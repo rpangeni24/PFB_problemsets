64b9a87ff05339ea9bf85ea536b3f443ce898105
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-15 12:05:35 -0400

    updating file

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3 
 my_name = 'My name is Raj, working on Florida, now taking Programing for Biology course in CSHL.'
 print(my_name)
+my_favorite_color = 'Green'
+print(my_favorite_color)
 
