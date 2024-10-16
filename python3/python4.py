15be308878720de4c3d188301af6fb7f626d0ab1
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-16 16:29:01 -0400

    updating files

---
+++ python3/python4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#This file is for python problems set 4 on day3. 
+my_fav = ['reading', 'writing', 'coding', 'musics', 'walking']
+print(my_fav)
+
+#getting middle element in the list
+my_fav[2]
+#creating a different variable for middle element
+middle_ele = my_fav[2]
+print(middle_ele)
+
+#replacing middle element with a new element
+
+my_fav[2] = 'pythoning'
+print(my_fav)
+
+my_fav.append('running')
+print(my_fav)
+my_fav.insert(0,'sleeping')
+print(my_fav)
+my_fav.pop()
+print(my_fav)
+my_fav.pop(0)
+print(my_fav)
+my_fav.pop(2)
+print(my_fav)
+
