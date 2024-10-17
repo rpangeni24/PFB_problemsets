21fe4f89e7d8016972839e59f91204e28dfe180c
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-16 21:52:15 -0400

    file updating

---
+++ python3/python4.2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#files for question 13 onwards
+
+#Question 13
+
+dnaseqs = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
+
+for dnaseq in dnaseqs:
+        print(dnaseq)
+
