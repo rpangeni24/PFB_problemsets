4bf88c7362d8fb0606bd70644e4276ddd76eb917
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-17 11:20:04 -0400

    new files updating

---
+++ python4/python5.1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#working with problemsets 5
+
+my_fav = {'book': 'jitterbug perfume', 'song':"Tom petty-I won't back down", 'tree': 'cedar'}
+print(my_fav)
+
+#printing a key from the dictionary
+
+print(my_fav['book'])
+
+#pringing out favourite book but using a variable key
+
+fav_thing = 'book'
+print(my_fav[fav_thing])
+
+#printing another key from a dictionary
+
+print(my_fav['tree'])
+
+#adding new key to the disctionary
+
+my_fav[organism] = Ecoli
+
+print(my_fav)
+
