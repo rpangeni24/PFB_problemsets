3cbb54636c522d8bacc67a9bfe746395d8e5aabb
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-18 13:56:03 -0400

    added files

--- python5/python7.2.py
+++ python5/python7.2.py
@@ -2,4 +2,18 @@
 
 #question 2 of set7
 
+import re
+
+count = 0
+my_txt = open("Python_07_nobody.txt", "r")
+ 
+for line in my_txt: 
+	line = line.rstrip()
+	if re.search(r"Nobody",line):
+		new_file = line.replace('Nobody','Rashi')	
+		print(new_file)
+		
+
+#FIRST TIME SOLVED THIS QUESTION IN THE FIRST GO WITHOTU ANY PROBLEM. :)
+
 
