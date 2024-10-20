88f91e76781159c177e61680f1d23b188d15c779
pfb2024 <pfb2024@info20.cshl.edu>
2024-10-20 13:54:01 -0400

    adding new file

---
+++ python5/python7.7m2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+   
+#Working with questionsets 7, question 7 method2
+import re
+seq7 = open("Python_07_ApoI.fasta", "r")
+apo_rsites = r"(A|G)AATT(C|T)"
+for seq in seq7:
+    seq = seq.rstrip()
+    if re.search(r"[GA]AATT[CT]",seq):
+        seq_cut = re.sub(apo_rsites,r"\1AATT^\2", seq)
+        #print(seq_cut)
+
+        cutsites = seq_cut.split("^")
+        print(cutsites)
+
+        #for fragments in cutsites:
+            #frag_length = [len(fragments)]
+        sorted_frag = sorted(cutsites)
+        print(sorted_frag)
+
+
